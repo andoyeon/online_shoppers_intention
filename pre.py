bf3b7f6868f9f16f97dadeef5dfece367915a2bd
andoyeon <56617914+andoyeon@users.noreply.github.com>
2020-02-03 23:11:16 +0900

    .

--- pre.py
+++ pre.py
@@ -205,7 +205,7 @@ featureNames = ['Administrative', 'Administrative_Duration', 'Informational', 'I
                 'region1', 'region2', 'region3', 'region4', 'region5', 'region6', 'region7', 'region8', 'region9',
                 'TrafficType', 'VisitorType1', 'VisitorType2', 'VisitorType3', 'Weekend1', 'Weekend2', 'Revenue']
 df = pd.DataFrame(df_, columns=featureNames)
-print(df.head(1))
+# print(df.head(1))
 
 # Dividing X and y
 X = df.iloc[:, :-1]
